6be83109a3e9d61cd39af2b72ea53746ef519eec
toshiXyasu <93040342+toshiXyasu@users.noreply.github.com>
2022-03-08 18:46:09 +0400

    wazSXDCF

---
+++ Sports/sportani.js
@@ -0,0 +1,3 @@
+gsap.from('.BGCircle4',  {delay: 0.5,x:-300, duration: 1, ease: 'expo.inOut', opacity:0})
+gsap.from('.homebutton',  {delay: 1,x:-400, duration: 1, ease: 'expo.inOut', opacity:0})
+gsap.from('.options',  {delay: 1.7,y:-200, duration: 1, ease: 'expo.inOut', opacity:0})
